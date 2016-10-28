6385560bfbee387c5ff84b2ddd5394162ed22080
berndtlindner <berndtlindner@gmail.com>
2016-10-28 16:17:59 +0200

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,7 +1,7 @@
 shinyUI(bootstrapPage(
 
   selectInput(inputId = "n_breaks",
-              label = "Number of bins in histogram (approximate):",
+              label = "Test! Number of bins in histogram (approximate):",
               choices = c(10, 20, 35, 50),
               selected = 20),
 
